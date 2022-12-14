a3196fa6936ad5fc681fcc80aca6e64ac1f9d58c
shellby.miller <shellby.miller@gmail.com>
2022-12-13 21:49:27 -0500

    added inquirer require

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +9,10 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.createPromptModule(questions)
+    
+}
 
 // Function call to initialize app
 init();
