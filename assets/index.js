3d90b30622e2028723a9479999dd319ec2ad50b5
shellby.miller <shellby.miller@gmail.com>
2022-12-15 20:15:47 -0500

    added questions array

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,46 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const markdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please provide a description of your project.',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What is the installation process?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'How will this project be used?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Who are the contributors to your project?',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'What is the test process?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'What licenses are required?',
+        name: 'licenses',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
